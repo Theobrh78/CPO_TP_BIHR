375f8286d18d954100f36758f6254dd4303e9c53
User <User@LAPTOP-A9T0OSJ4.education.lakanal.epf.fr>
2022-10-21 16:12:21 +0200

    nouvelle version

---
+++ TP2_Bieres_BIHR/src/main/java/com/mycompany/tp2_bieres_bihr/TP2_Bieres_BIHR.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tp2_bieres_bihr;
+
+/**
+ *
+ * @author User
+ */
+public class TP2_Bieres_BIHR {
+
+    public static void main(String[] args) {
+        BouteilleBiere uneBiere = new BouteilleBiere() ;
+    }
+}
+
