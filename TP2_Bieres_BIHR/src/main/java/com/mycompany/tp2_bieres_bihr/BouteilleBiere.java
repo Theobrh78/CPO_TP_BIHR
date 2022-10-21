5c33ce86fc3e4d6d2955295a925ec1ce5f21dcfb
User <User@LAPTOP-A9T0OSJ4.education.lakanal.epf.fr>
2022-10-21 16:34:26 +0200

    nouvelle version

---
+++ TP2_Bieres_BIHR/src/main/java/com/mycompany/tp2_bieres_bihr/BouteilleBiere.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tp2_bieres_bihr;
+
+/**
+ *
+ * @author User
+ */
+public class BouteilleBiere {
+    String nom ;
+    double degreAlcool ;
+    String brasserie;
+    boolean ouverte;
+
+    
+    public void lireEtiquette() {
+    System.out.println("Bouteille de " + nom +" (" + degreAlcool +
+ " degres) \nBrasserie : " + brasserie ) ;
+        
+    }
+    
+  
+}
+    
+    
+            
+
+
+
+
