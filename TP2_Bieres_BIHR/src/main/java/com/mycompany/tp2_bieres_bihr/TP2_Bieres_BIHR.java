47d8ef34f13aa22861cb019f36f7a605a4afce54
User <User@LAPTOP-A9T0OSJ4.education.lakanal.epf.fr>
2022-10-21 16:54:30 +0200

    nouvelle version

--- TP2_Bieres_BIHR/src/main/java/com/mycompany/tp2_bieres_bihr/TP2_Bieres_BIHR.java
+++ TP2_Bieres_BIHR/src/main/java/com/mycompany/tp2_bieres_bihr/TP2_Bieres_BIHR.java
@@ -12,20 +12,20 @@ package com.mycompany.tp2_bieres_bihr;
 public class TP2_Bieres_BIHR {
 
     public static void main(String[] args) {
-        BouteilleBiere uneBiere = new BouteilleBiere() ;
-        uneBiere.nom = "rince cochon";
-        uneBiere.degreAlcool = 65 ;
-        uneBiere.brasserie = "Dubuisson";
-        uneBiere.ouverte = false;
-           uneBiere.lireEtiquette();
+     BouteilleBiere autreBiere = new BouteilleBiere("Leffe", 6.6,"Abbaye de Leffe") ;
+        //uneBiere.nom = "rince cochon";
+        //uneBiere.degreAlcool = 65 ;
+        //uneBiere.brasserie = "Dubuisson";
+        //uneBiere.ouverte = false;
+          // uneBiere.lireEtiquette();
            
-        BouteilleBiere uneBiere1 = new BouteilleBiere() ;
-        uneBiere1.nom = "Leffe";
-        uneBiere1.degreAlcool = 6.6 ;
-        uneBiere1.brasserie = "Abbaye de Leffe";
-        uneBiere1.ouverte = false;
-           uneBiere1.lireEtiquette();   
+        //BouteilleBiere uneBiere1 = new BouteilleBiere() ;
+        //uneBiere1.nom = "Leffe";
+        //uneBiere1.degreAlcool = 6.6 ;
+        //uneBiere1.brasserie = "Abbaye de Leffe";
+        //uneBiere1.ouverte = false;
+       //    uneBiere1.lireEtiquette();   
     }
-    
-}
+
+
 
