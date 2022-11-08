50b2e56928919ac724dffc1a19a6dc3873a013cb
User <User@LAPTOP-A9T0OSJ4.education.lakanal.epf.fr>
2022-11-08 17:07:07 +0100

    creation des épées et batons

---
+++ TP_BIHR_Théophile/src/TP_BIHR_Théophile.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+
+/**
+ *
+ * @author User
+ */
+public class TP_BIHR_Théophile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    epee Ex = new epee("Excalibure", 7, 5);
+    epee Du = new epee("Durandal", 4, 7);
+    
+    Baton Ch = new Baton ("Chene",4,5);
+    Baton Cha = new Baton ("Charme",5,6);
+    
+    
+}
