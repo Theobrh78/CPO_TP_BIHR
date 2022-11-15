1db098c96df702ddaf9e3c91e142a1a249c3572c
User <User@LAPTOP-A9T0OSJ4.education.lakanal.epf.fr>
2022-11-15 12:10:43 +0100

    affichage depuis le tableau des epees et des batons

--- TP_BIHR_Théophile/src/TP_BIHR_Théophile.java
+++ TP_BIHR_Théophile/src/TP_BIHR_Théophile.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -15,12 +18,23 @@ public class TP_BIHR_Théophile {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+    
     epee Ex = new epee("Excalibure", 7, 5);
     epee Du = new epee("Durandal", 4, 7);
     
     Baton Ch = new Baton ("Chene",4,5);
     Baton Cha = new Baton ("Charme",5,6);
     
+    ArrayList <Arme> Tab = new ArrayList ();
+    Tab.add(Ex);
+    Tab.add(Du);
+    Tab.add(Ch);
+    Tab.add(Cha);
     
+    int taille = Tab.size();
+    for (int i=0; i<taille; i++ ){
+        System.out.println(Tab.get(i));
+    }
+    
+    }   
 }
