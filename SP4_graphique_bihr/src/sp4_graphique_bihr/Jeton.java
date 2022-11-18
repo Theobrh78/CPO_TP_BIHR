867a244e0fe2d10c8c08b264479366564adf1bc4
User <User@LAPTOP-A9T0OSJ4.education.lakanal.epf.fr>
2022-11-18 11:30:12 +0100

    creation du projet, et début de la classe jeton

---
+++ SP4_graphique_bihr/src/sp4_graphique_bihr/Jeton.java
@@ -0,0 +1,25 @@
+package sp4_graphique_bihr;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+
+public class Jeton {
+  String couleur;
+    public Jeton(String couleur) {
+        this.couleur = couleur;
+    }
+
+    
+    
+   
+    
+       
+  
+}
