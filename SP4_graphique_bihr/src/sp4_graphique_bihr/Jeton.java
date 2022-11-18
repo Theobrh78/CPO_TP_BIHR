fbf169c0b631751e25f479ae9a79c5d0319bc831
User <User@LAPTOP-A9T0OSJ4.education.lakanal.epf.fr>
2022-11-18 12:59:58 +0100

    classe jeton terminé

--- SP4_graphique_bihr/src/sp4_graphique_bihr/Jeton.java
+++ SP4_graphique_bihr/src/sp4_graphique_bihr/Jeton.java
@@ -11,15 +11,29 @@ package sp4_graphique_bihr;
  */
 
 public class Jeton {
-  String couleur;
+    private String couleur;
     public Jeton(String couleur) {
         this.couleur = couleur;
     }
 
+    public String lireCouleur() {
+        return couleur;
+    }  
     
-    
-   
-    
+@Override 
+    public String toString() {
+    String C="";
+    String chaine_a_retourner;
+    if (couleur=="rouge"){
+    C="R";  
+    }
+    if (couleur=="jaune"){
+    C="J";    
+    }
+    chaine_a_retourner = "Couleur du Jeton: " + C ;
+    return chaine_a_retourner;
+    }
        
-  
 }
+
+
