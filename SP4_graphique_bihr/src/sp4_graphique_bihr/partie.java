d5a5f180bea09f4659dcd3717c45bc01a9aa0a57
User <User@LAPTOP-A9T0OSJ4.education.lakanal.epf.fr>
2022-12-05 17:24:36 +0100

    je reessaye de faire la méthode lancer partie sinon je ne comprendrai pas les subtilités du tp

--- SP4_graphique_bihr/src/sp4_graphique_bihr/partie.java
+++ SP4_graphique_bihr/src/sp4_graphique_bihr/partie.java
@@ -97,128 +97,28 @@ public class partie {
         placerTrousNoirsEtDesintegrateurs();
     }
     
-    public void lancerpartie(){
-    joueurCourant = listeJoueurs[0];//le joueur classé en premier comence
-        joueurSuivant = listeJoueurs[1];
-        boolean finito = false;
-        while (finito == false) { //tant que la partie n'est pas finie
-            Scanner sc;
-            int colonneJouee;
-            int choixJoueur;
-            sc = new Scanner(System.in);
-            plateau.afficherGrilleSurConsole();
-            if (joueurCourant == listeJoueurs[0]) {
-                joueurSuivant = listeJoueurs[1];
-            } else if (joueurCourant == listeJoueurs[1]) {
-                joueurSuivant = listeJoueurs[0];//on inverse le joueur courant
-            }
-            System.out.println("Au tour de " + joueurCourant + " (couleur " + joueurCourant.lireCouleur() + ")");
-            
-
-            choixJoueur = 99;   
-            while (choixJoueur != 1 && choixJoueur != 2 && choixJoueur != 3) { //tant que le choix du joueur n'est pas valide
-                System.out.println("Que voulez vous faire?");
-                System.out.println("Placer un jeton (1), en recuperer un (2) ou jouer un desintegrateur (3)?");
-                choixJoueur = sc.nextInt();
-            }
-        ArrayList<Jeton> reserveJetons;
-
-            while (choixJoueur == 2 && joueurCourant.setReserveJetons(reserveJetons).size() == 30) {
-                System.out.println("Vous n'avez pas de jeton a retirer");
-                System.out.println("Que voulez vous faire?");
-                System.out.println("Placer un jeton (1), en recuperer un (2) ou jouer un desintegrateur (3)?");
-                choixJoueur = sc.nextInt();
-            }
-
-            while (choixJoueur == 3 && joueurCourant.getNombreDesintegrateurs() == 0) {
-                System.out.println("Vous n'avez pas de desintegrateur. Gros chien");
-                System.out.println("Que voulez vous faire?");
-                System.out.println("Placer un jeton (1), en recuperer un (2) ou jouer un desintegrateur (3)?");
-                choixJoueur = sc.nextInt();
-
-            }
-
-///si le joueur joue un jeton
-
-            if (choixJoueur == 1) {
-                System.out.println("Sur quelle colonne voulez-vous jouer? (1 a 7)");
-                colonneJouee = sc.nextInt(); //saisie sur l'interface
-                while ((colonneJouee <= 0) || (colonneJouee >= 8)) {  //tant que l'utilisateur comprend que dalle, on lui redemande
-                    System.out.println("Sur quelle colonne voulez-vous jouer? (1 a 7)");
-                    colonneJouee = sc.nextInt();
-                }
-
-                int ligneJouee;
-                ligneJouee = plateau.ajouterJetonDansColonne(joueurCourant.getReserveJetons().get(0), colonneJouee - 1);
-                while (ligneJouee == 99) { //tant que le joueur joue sur une colonne pleine, on lui demande de rejouer
-                    System.out.println("Erreur: la colonne est pleine");
-                    System.out.println("Sur quelle colonne voulez-vous jouer? (1 a 7)");
-                    colonneJouee = sc.nextInt(); //saisie sur l'interface
-                    ligneJouee = plateau.ajouterJetonDansColonne(joueurCourant.getReserveJetons().get(0), colonneJouee - 1);
-                }
-                if (plateau.presenceTrouNoir(ligneJouee, colonneJouee - 1)) {//si le joueur a joue dans un trou noir
-                    plateau.supprimerJeton(ligneJouee, colonneJouee - 1);
-                    plateau.supprimerTrouNoir(ligneJouee, colonneJouee - 1);
-                    System.out.println("Jeton mange par un trou noir. Cheh.");
-                }
-                if (plateau.presenceDesintegrateur(ligneJouee, colonneJouee - 1)) {
-                    plateau.supprimerDesintegrateur(ligneJouee, colonneJouee - 1);
-                    joueurCourant.obtenirDesintegrateur();
-                    System.out.println("Vous avez recupere un desintagrateur");
-                }
-                System.out.println("Pion place");
-                joueurCourant.jouerJeton();//si le colonne n'est pas pleine, on enleve le jeton place de la reserve du joueur
-            }
-/// si le joueur recupere un jeton
-            if (choixJoueur == 2) {
-                int ligneJouee;
-                System.out.println("Sur quelle colonne est le jeton que vous voulez enlever?");
-                colonneJouee = sc.nextInt();
-                System.out.println("Sur quelle ligne est le jeton que vous voulez recuperer?");
-                ligneJouee = sc.nextInt();
-                while (plateau.lireCouleurJeton(ligneJouee - 1, colonneJouee - 1) != joueurCourant.getCouleur()) { //tant que le joueur ne joue pas sur un jeton qu'il possede
-                    System.out.println("Il n'y a pas de jeton a vous sur  cette case.");
-                    System.out.println("Sur quelle colonne est le jeton que vous voulez enlever?");
-                    colonneJouee = sc.nextInt();
-                    System.out.println("Sur quelle ligne est le jeton que vous voulez recuperer?");
-                    ligneJouee = sc.nextInt();
-                }
-                joueurCourant.ajouterJeton(plateau.recupererJeton(ligneJouee - 1, colonneJouee - 1)); //on enleve le jeton et on le redonne au joueur
-                    plateau.tasserColonne(colonneJouee - 1);
-            }
-/// si le joueur joue un desintegrateur
-            if (choixJoueur == 3) {
-                int ligneDesint;
-                System.out.println("Sur quelle colonne est le jeton que vous voulez desintegrer?");
-                colonneJouee = sc.nextInt();
-                System.out.println("Sur quelle ligne est le jeton que vous voulez desintegrer?");
-                ligneDesint = sc.nextInt();
-                while (plateau.lireCouleurJeton(ligneDesint - 1, colonneJouee - 1) != joueurSuivant.getCouleur()) {
-                    System.out.println("Il n'y a pas de jeton advesre sur  cette case.");
-                    System.out.println("Sur quelle colonne est le jeton que vous voulez desintegrer?");
-                    colonneJouee = sc.nextInt();
-                    System.out.println("Sur quelle ligne est le jeton que vous voulez desintegrer?");
-                    ligneDesint = sc.nextInt();
-                }
-                plateau.supprimerJeton(ligneDesint-1, colonneJouee-1);
-                joueurCourant.utiliserDesintegrateur();
-                plateau.tasserColonne(colonneJouee-1);
-            }
-///
-
-            if (plateau.etreGagnantePourCouleur(joueurSuivant.getCouleur())) { //on verifie que la victoire ne revienne pas a l'autre joueur suit un la suppression d'un jeton
-                System.out.println("Felicitations, le joueur " + joueurSuivant + " a gagne");
-                finito = true;
-                plateau.afficherGrilleSurConsole();
-            } else if (plateau.etreGagnantePourCouleur(joueurCourant.getCouleur())) {
-                finito = true;
-                plateau.afficherGrilleSurConsole();
-                System.out.println("Felicitations, le joueur " + joueurCourant + " a gagne");
-            }
-            if (plateau.grilleRemplie()) {
-                System.out.println("La grille est pleine: fin de partie");
-                finito = true;
-            }
-            joueurCourant = joueurSuivant;
+    public void lancerpartie(joueur J1, joueur J2){
+        boolean partieencour = true;
+        initialiserPartie();
+        int p;
+        int i = 0;
+        int mode;
+        int colonne;
+        while (partieencour==true){
+            p=i%2;
+            joueurCourant=listeJoueurs[p];
+            i=i+1;
+            System.out.println(joueurCourant + "peut jouer");
+            System.out.println("entrez 1 si vous voulez palcer un jeton :");
+            Scanner sc=new Scanner(System.in);
+            mode = sc.nextInt();
+                    
+                    if (mode==1){
+                        System.out.println("indiquez la colone sur laquelle vous voulez placer un jeton");
+                        colonne=sc.nextInt();
+                        plateau.ajouterJetonDansColonne(colonne);
+                        plateau.
+                    }
         }
- }
+    }
+    }
