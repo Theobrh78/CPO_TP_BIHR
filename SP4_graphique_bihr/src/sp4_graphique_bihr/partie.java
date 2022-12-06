7c99a1512e6eb249014ec3c056fdaf96bc9d14ff
User <User@LAPTOP-A9T0OSJ4.education.lakanal.epf.fr>
2022-12-06 10:15:56 +0100

    la grille de jeux s'affiche sans trous noirs et desintegrateurs mais toutes les ubtilités ne sont pas encore correct

--- SP4_graphique_bihr/src/sp4_graphique_bihr/partie.java
+++ SP4_graphique_bihr/src/sp4_graphique_bihr/partie.java
@@ -26,24 +26,24 @@ public class partie {
     }    
     
     
-    public void attribuerCouleurAuxJoueurs(){
+    public void attribuerCouleurAuxJoueurs(String j1, String j2){
        Random r = new Random();
        int a = r.nextInt(2);
        if (a==1){
            listeJoueurs[0].affecterCouleur("rouge");
-            System.out.println("le joueur"+listeJoueurs[0]+"a les rouges");
+            System.out.println("le joueur "+ j1 +" a les rouges");
        }
        else {
            listeJoueurs[1].affecterCouleur("jaune");
-           System.out.println("le joueur"+listeJoueurs[1]+"a les jaunes");
+           System.out.println("le joueur "+ j2 +" a les jaunes");
        } 
        if (a==2){
            listeJoueurs[1].affecterCouleur("rouge");
-           System.out.println("le joueur"+listeJoueurs[1]+"a les rouges");
+           System.out.println("le joueur "+ j2 +" a les rouges");
        }
        else {
            listeJoueurs[0].affecterCouleur("jaune");
-           System.out.println("le joueur"+listeJoueurs[0]+"a les jaunes");
+           System.out.println("le joueur "+ j1 +" a les jaunes");
     }
     }
     
@@ -60,7 +60,7 @@ public class partie {
         int a = r.nextInt(6);
         Random c = new Random();
         int b = c.nextInt(7);
-            if(plateau.presenceTrouNoir(a, b)==false&&plateau.presenceDesintegrateur(a, b)==false){
+            if((plateau.presenceTrouNoir(a, b)==false) && (plateau.presenceDesintegrateur(a, b)==false)){
              plateau.placerTrouNoir(a-1, b-1);
              plateau.placerDesintegrateur(a-1, b-1);
              i=i+1;
@@ -91,24 +91,35 @@ public class partie {
     }
     
    
-    public void initialiserPartie(){
-        attribuerCouleurAuxJoueurs();
-        creerEtAffecterJeton(joueurCourant);
-        placerTrousNoirsEtDesintegrateurs();
+    public void initialiserPartie(String j1, String j2){
+        attribuerCouleurAuxJoueurs(j1,j2);
+        creerEtAffecterJeton(listeJoueurs[0]);
+        creerEtAffecterJeton(listeJoueurs[1]);
+        //placerTrousNoirsEtDesintegrateurs();
     }
     
     public void lancerpartie(joueur J1, joueur J2){
         boolean partieencour = true;
-        initialiserPartie();
+        
         int p;
         int i = 0;
         int mode;
         int colonne;
+        String Nomjoueur="";
+        Jeton jetoncourant;
         while (partieencour==true){
             p=i%2;
             joueurCourant=listeJoueurs[p];
             i=i+1;
-            System.out.println(joueurCourant + "peut jouer");
+            if (p==0){
+                Nomjoueur=J1.lireCouleur();
+            }
+            if (p==1){
+                Nomjoueur=J2.lireCouleur(); 
+            }
+            System.out.println(Nomjoueur +  " peut jouer");
+            System.out.println("pour info, voici l'etat de la grille de jeu :");
+            plateau.afficherGrilleSurConsole();
             System.out.println("entrez 1 si vous voulez palcer un jeton :");
             Scanner sc=new Scanner(System.in);
             mode = sc.nextInt();
@@ -116,9 +127,18 @@ public class partie {
                     if (mode==1){
                         System.out.println("indiquez la colone sur laquelle vous voulez placer un jeton");
                         colonne=sc.nextInt();
-                        plateau.ajouterJetonDansColonne(colonne);
-                        plateau.
+                        jetoncourant=joueurCourant.jouerJeton();
+                        plateau.ajouterJetonDansColonne(colonne-1, jetoncourant);
+                        System.out.println("il vous restes" + joueurCourant.nombreDeJetons() + "jetons");               
                     }
-        }
+                    if (plateau.coloneGagnantePourCouleur(joueurCourant.lireCouleur())==true||plateau.ligneGagnantePourCouleur(joueurCourant.lireCouleur())==true||plateau.diagonaleDesencanteGagnantePourCouleur(joueurCourant.lireCouleur())==true||plateau.diagonaleMontanteGagnantePourCouleur(joueurCourant.lireCouleur())==true){
+                       System.out.println("bravo 1" + Nomjoueur + " a gagné !");
+                        partieencour=false;
+                    }
+                    
+                    
+        
+                        }
+        
     }
     }
